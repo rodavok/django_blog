bea7eabeff5b235eb1edce340f7710dc63f19523
rodavok <rodavok@DESKTOP-134UEK0.localdomain>
2022-09-23 20:55:19 -0400

    permissions

--- django_project/settings.py
+++ django_project/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
 
 CORS_ORIGIN_WHITELIST = (
