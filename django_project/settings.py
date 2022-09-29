56f45a8345afd6db32d42c72d0bd96a27eeaeaa5
rodavok <rodavok@DESKTOP-134UEK0.localdomain>
2022-09-28 22:01:33 -0400

    documentation

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "dj_rest_auth.registration",
+    "drf_spectacular",
     # Local
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
